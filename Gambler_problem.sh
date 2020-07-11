33c664f1d69e2b2bc4cd7ee727ca61acc72a0e23
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-11 09:49:22 +0530

    [Mounika]Added WinorLoose condition

--- Gambler_problem.sh
+++ Gambler_problem.sh
@@ -2,3 +2,16 @@
 echo "Wellcome to Gambling Simulation"
 STAKE_AMOUNT=100
 bet_rupees_per_game=1
+WIN=1
+function play()
+            {
+                bet=$(( RANDOM % 2 ))
+                        if [[ $bet -eq $WIN ]]
+                        then
+                             echo "win"
+                        else
+                              echo "loose"
+                        fi
+           }
+ play
+
