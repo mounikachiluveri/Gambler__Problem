1c95ceeb220576f15f8c9dd73bbe31e98c14b73c
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-11 10:17:18 +0530

    [Mounika] Added:Checking the totalamount win or loose after 20 day

--- Gambler_problem.sh
+++ Gambler_problem.sh
@@ -5,9 +5,15 @@ bet_rupees_per_game=1
 WIN=1
 current_amount=0
 PERCENTAGE=50
+amount=0
+NUMBER_OF_DAYS=20
+maximumAmount=0
 function play()
             {
-            current_amount=STAKE_AMOUNT
+              declare -a day
+                 for((dayOfMonth=1;dayOfMonth<=NUMBER_OF_DAYS;dayOfMonth++))
+                 do
+                       current_amount=STAKE_AMOUNT
                      while((1))
                      do
                         bet=$(( RANDOM % 2 ))
@@ -20,9 +26,20 @@ function play()
 
                             if [[ $current_amount -eq $(($(($STAKE_AMOUNT*$PERCENTAGE))/100)) || $current_amount -eq $(($STAKE_AMOUNT+$(($(($STAKE_AMOUNT*$PERCENTAGE))/100)))) ]]
                             then
-                                  echo "resign for the day"
+                                  amount=$(($amount+$current_amount))
+                                  day[$dayOfMonth]=$current_amount
                                   break;
-                            fi
-                   done
-         }
-      play
+                             fi
+                    done
+               done
+
+              maximumAmount=$(($STAKE_AMOUNT*$NUMBER_OF_DAYS))
+       if [[ $amount -lt $maximumAmount ]]
+       then
+             echo "Gambler Lost by $(($maximumAmount-$amount))"
+       else
+             echo "Gambler Won by $(($amount-$maximumAmount))$"
+       fi
+   }
+     play
+       
