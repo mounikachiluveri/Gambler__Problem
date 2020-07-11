0382f7139363045510d1816dec535b5ba91e2ebd
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-11 10:35:56 +0530

    [Mounika]:Refactor the code for month,display the amount daywise and winandloose amount in daily wise

--- Gambler_problem.sh
+++ Gambler_problem.sh
@@ -1,18 +1,21 @@
 #!/bin/bash -x
 echo "Wellcome to Gambling Simulation"
-STAKE_AMOUNT=100
-bet_rupees_per_game=1
-WIN=1
-current_amount=0
 PERCENTAGE=50
+STAKE_AMOUNT=100
+NUMBER_OF_DAYS=30
+daysWon=0
+daysLost=0
+WIN=1
+bet_rupees_per_game=1
+current_amount=0
 amount=0
-NUMBER_OF_DAYS=20
 maximumAmount=0
 function play()
             {
-              declare -a day
-                 for((dayOfMonth=1;dayOfMonth<=NUMBER_OF_DAYS;dayOfMonth++))
-                 do
+            declare -a day
+            declare -a day1
+            for((dayOfMonth=1;dayOfMonth<=NUMBER_OF_DAYS;dayOfMonth++))
+            do
                        current_amount=STAKE_AMOUNT
                      while((1))
                      do
@@ -26,20 +29,25 @@ function play()
 
                             if [[ $current_amount -eq $(($(($STAKE_AMOUNT*$PERCENTAGE))/100)) || $current_amount -eq $(($STAKE_AMOUNT+$(($(($STAKE_AMOUNT*$PERCENTAGE))/100)))) ]]
                             then
-                                  amount=$(($amount+$current_amount))
-                                  day[$dayOfMonth]=$current_amount
-                                  break;
+                             amount=$(($amount+$current_amount))
+                             day[$dayOfMonth]=$current_amount
+                             day1[$dayOfMonth]=$amount
+
+                             break;
                              fi
                     done
-               done
+                if [[ $current_amount -gt $STAKE_AMOUNT ]]
+                then
+                        let daysWon++
+                        echo "Day $dayOfMonth won  : $current_amount$ : Total amount:$amount"
+                else
+                        let daysLost++
+                        echo "Day $dayOfMonth loss  : $current_amount$ : Total amount: $amount"
+                fi
+                check=${day[$dayOfMonth]}
 
-              maximumAmount=$(($STAKE_AMOUNT*$NUMBER_OF_DAYS))
-       if [[ $amount -lt $maximumAmount ]]
-       then
-             echo "Gambler Lost by $(($maximumAmount-$amount))"
-       else
-             echo "Gambler Won by $(($amount-$maximumAmount))$"
-       fi
-   }
-     play
-       
+          done
+                        
+         }
+play
+
