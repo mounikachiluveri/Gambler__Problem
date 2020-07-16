6ba2e28214e9268878733241aa21ae5e3d2ba99c
Mounika-chiluveri <chiluveri.mounika@gmail.com>
2020-07-16 19:55:27 +0530

    [Mounika] Refactor:Code Refactored

--- Gambler_problem.sh
+++ Gambler_problem.sh
@@ -13,126 +13,128 @@ bet_rupees_per_game=1
 current_amount=0
 amount=0
 maximumAmount=0
-    function lukiestOrunlukiest()
-         {
-        declare -a lucky
-        declare -a unlucky
-        luckyDayKey=0
-        unluckyDayKey=0
-        for (( dayOfMonth=1; dayOfMonth<=30; dayOfMonth++ ))
-        do
-                if [[ ${day[$dayOfMonth]} == $maximum ]]
-                then
-                        lucky[$luckyDayKey]=$dayOfMonth
-                        let luckyDayKey++
-
-
-                elif [[ ${day[$dayOfMonth]} -gt $maximum ]]
-                then
-
-                        lucky=( )
-                        maximum=${day[$dayOfMonth]}
-                        luckyDayKey=0
-                        lucky[$luckyDayKey]=$(($dayOfMonth))
-                        let luckyDayKey++
-                fi
-
-                if [[ ${day[$dayOfMonth]} == $minimum ]]
-                then
-                        unlucky[$unluckyDayKey]=$dayOfMonth
-                        let unluckyDayKey++
-                elif [[ ${day[$dayOfMonth]} -lt $minimum ]]
-                then
-                        unlucky=( )
-
-  minimum=${day[$dayOfMonth]}
-                        unluckyDayKey=0
-                        unlucky[$unluckyDayKey]=$(($dayOfMonth))
-                        let unluckyDayKey++
-                fi
-
-        done
-        for key in ${lucky[@]}
-        do
-                echo "Lukiest day is $key"
-        done
-        echo
-        for key2 in ${unlucky[@]}
-        do
-                echo "Unlukiest day is $key2"
-        done
-        }
-
+function lukiestOrunlukiest(){
+     declare -a lucky
+     declare -a unlucky
+     luckyDayKey=0
+     unluckyDayKey=0
+       for (( dayOfMonth=1; dayOfMonth<=NUMBER_OF_DAYS; dayOfMonth++ ))
+       do
+           if [[ ${day[$dayOfMonth]} == $maximum ]]
+           then
+           lucky[$luckyDayKey]=$dayOfMonth
+           let luckyDayKey++
+           elif [[ ${day[$dayOfMonth]} -gt $maximum ]]
+           then
+           lucky=( )
+           maximum=${day[$dayOfMonth]}
+           luckyDayKey=0
+           lucky[$luckyDayKey]=$(($dayOfMonth))
+           let luckyDayKey++
+           fi
+           if [[ ${day[$dayOfMonth]} == $minimum ]]
+           then
+           unlucky[$unluckyDayKey]=$dayOfMonth
+           let unluckyDayKey++
+           elif [[ ${day[$dayOfMonth]} -lt $minimum ]]
+           then
+           unlucky=( )
+           minimum=${day[$dayOfMonth]}
+           unluckyDayKey=0
+           unlucky[$unluckyDayKey]=$(($dayOfMonth))
+           let unluckyDayKey++
+           fi
+      done
+      for key in ${lucky[@]}
+      do
+      echo "Lukiest day is $key"
+      done
+      echo
+      for key2 in ${unlucky[@]}
+      do
+      echo "Unlukiest day is $key2"
+      done
+}
 
 function continueOrStop()
 {
-
-     while [[ $start -gt 0 ]]
-     do
-        echo "Total Amount is "$amount
-        maximumAmount=$(($STAKE_AMOUNT*$NUMBER_OF_DAYS))
-        if [[ $amount -lt $maximumAmount ]]
-        then
-                echo "Gambler Lost by $(($maximumAmount-$amount))"
-                start=-1;
-        else
-                echo "Gambler Won by $(($amount-$maximumAmount))$"
-                          echo "You won $(($amount-$maximumAmount)), do you want to continue ?"
-                          read -p "Press 1 for Yes 2 for No : " continue
-                          case $continue in
-                                             1)
-                                                 play
-                                                 ;;
-                                             2)
-                                                 start=-1
-                                                 break
-                                                 ;;
-                                             *)
-                                                 echo "Enter valid input"
-                                                 ;;
-                esac
-        fi
+   while [[ $start -gt 0 ]]
+   do
+   maximumAmount=$(($STAKE_AMOUNT*$NUMBER_OF_DAYS))
+     if [[ $totalAmount -lt $maximumAmount ]]
+     then
+     echo "Gambler Lost by $(($maximumAmount-$totalAmount))"
+     start=-1;
+     else
+     echo "Gambler Won by $(($totalAmount-$maximumAmount))$"
+     echo "You won $(($totalAmount-$maximumAmount)), do you want to continue ?"
+     read -p "Press 1 for Yes 2 for No : " continue
+        case $continue in
+             1)
+                play
+                ;;
+             2)
+                start=-1
+                break
+                ;;
+             *)
+               echo "Enter valid input"
+               ;;
+         esac
+     fi
  done
 }
 
+function play(){
+   declare -a winamount
+   declare -a looseamount
+   declare -a day
+   declare -a day1
+   for((dayOfMonth=1;dayOfMonth<=NUMBER_OF_DAYS;dayOfMonth++))
+   do
+   currentAmount=$(($currentAmount + $STAKE_AMOUNT))
+   Percentage=$(($currentAmount/2))
+   lowerlimit=$(($currentAmount - $Percentage))
+   upperlimit=$(($currentAmount + $Percentage))
+     while((1))
+     do
+     bet=$(( RANDOM % 2 ))
+       if [[ $bet -eq $WIN ]]
+       then
+       currentAmount=$(($currentAmount + $bet_rupees_per_game))
+       else
+       currentAmount=$(($currentAmount - $bet_rupees_per_game))
+       fi
+         if [[ $currentAmount -eq $lowerlimit || $currentAmount -eq $upperlimit ]]
+         then
+         #amount=$(($amount+$current_amount))
+         day[$dayOfMonth]=$currentAmount
+         day1[$dayOfMonth]=$amount
+         break;
+         fi
+     done
+ if [[ $currentAmount -gt $(($STAKE_AMOUNT * $dayOfMonth)) ]]
+         then
+         let daysWon++
+         echo "Day $dayOfMonth won  : $currentAmount$"
+         winAmount=$(($winAmount + $currentAmount))
+         else
+         let daysLost++
+         echo "Day $dayOfMonth loss  : $currentAmount$ "
+          looseAmount=$(($looseAmount + $currentAmount))
+         fi
+   done
+        echo " Winamount=$winAmount LooseAmount=$looseAmount"
+        if [[ $Winamount -gt $looseAmount ]]
+        then
+           totalAmount=$(($Winamount-$looseAmount))
+        else
+           totalAmount=$(($Winamount-$looseAmount))
+        fi
 
-function play()
-            {
-            declare -a day
-            declare -a day1
-            for((dayOfMonth=1;dayOfMonth<=NUMBER_OF_DAYS;dayOfMonth++))
-            do
-                       current_amount=STAKE_AMOUNT
-                     while((1))
-                     do
-                        bet=$(( RANDOM % 2 ))
-                        if [[ $bet -eq $WIN ]]
-                        then
-                                current_amount=$(($current_amount + $bet_rupees_per_game))
-                        else
-                                current_amount=$(($current_amount - $bet_rupees_per_game))
-                        fi
-
-                            if [[ $current_amount -eq $(($(($STAKE_AMOUNT*$PERCENTAGE))/100)) || $current_amount -eq $(($STAKE_AMOUNT+$(($(($STAKE_AMOUNT*$PERCENTAGE))/100)))) ]]
-                            then
-                             amount=$(($amount+$current_amount))
-                             day[$dayOfMonth]=$current_amount
-                             day1[$dayOfMonth]=$amount
-                                break;
-                             fi
-                    done
-                if [[ $current_amount -gt $STAKE_AMOUNT ]]
-                then
-                        let daysWon++
-                        echo "Day $dayOfMonth won  : $current_amount$:   Total amount:$amount"
-                else
-                        let daysLost++
-                        echo "Day $dayOfMonth loss  : $current_amount$ :  Total amount:$amount"
-                fi
-                check=${day[$dayOfMonth]}
-
-          done
-                        lukiestOrunlukiest
-                        continueOrStop
-         }
+lukiestOrunlukiest
+continueOrStop
+}
 play
+
+
